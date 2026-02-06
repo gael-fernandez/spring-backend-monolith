c8ce13dd955afa02fa1cf3616f60e761900a02ef
Gael Osbaldo Fernandez Garcia <gael.fernandez.dev@gmail.com>
2026-02-06 16:57:38 -0600

    chore: initial Spring Boot monolith setup

---
+++ src/main/java/com/gaelfernandez_monolith/SpringBackendMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.gaelfernandez_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendMonolithApplication.class, args);
+	}
+
+}
